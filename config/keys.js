8bd283117a4fed81b6b5deb132f8e1c18cce7bcc
ubaig517 <ubaig517@gmail.com>
2023-06-13 19:16:09 -0400

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// keys.js - determine credentials to use
+if (process.env.NODE_ENV === "production") {
+  // return prod keys
+  module.exports = require("./prod");
+} else {
+  // return dev keys
+  module.exports = require("./dev");
+}
---
+++ config/prod.js
@@ -0,0 +1,18 @@
+// prod.js - production keys
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+};
+
+/*
+    googleClientID:
+    "429088410295-pp1biq4mi6nfejgn576eva220r61tapo.apps.googleusercontent.com",
+    googleClientSecret: "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",
+    mongoURI:
+      "mongodb+srv://ubaig517:NQeyGlXYFPM0VcRh@cluster0.zehclz6.mongodb.net/emailyprod?retryWrites=true&w=majority",
+    cookieKey: "uZYDtaFE8cYNYThzn2vw",
+*/
