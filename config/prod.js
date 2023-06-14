6130e04db2c2f8be2c5d7ab86e69bd3a05a3a051
ubaig517 <ubaig517@gmail.com>
2023-06-13 20:29:57 -0400

    refactor

--- config/prod.js
+++ config/prod.js
@@ -7,12 +7,3 @@ module.exports = {
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 };
-
-/*
-    googleClientID:
-    "429088410295-pp1biq4mi6nfejgn576eva220r61tapo.apps.googleusercontent.com",
-    googleClientSecret: "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",
-    mongoURI:
-      "mongodb+srv://ubaig517:NQeyGlXYFPM0VcRh@cluster0.zehclz6.mongodb.net/emailyprod?retryWrites=true&w=majority",
-    cookieKey: "uZYDtaFE8cYNYThzn2vw",
-*/
